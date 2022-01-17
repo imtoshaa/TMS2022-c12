16f26241ec21c2f95c49edef4432d97c2b8c1a32
imtos <imtoshaa.work@gmail.com>
2022-01-17 22:00:09 +0300

    upd task1

--- Lesson1/src/main/java/Task1.java
+++ Lesson1/src/main/java/Task1.java
@@ -6,7 +6,7 @@ public class Task1 {
         int a;
         Scanner scanner = new Scanner(System.in); //включил считывание строки из консоли
         String str = scanner.nextLine(); //присвоил значение из консоли строке str
-        String arrstr[] = str.split(" ");//с помощью метода split разделил строку на состовляющие и заполнил массив String
+        String[] arrstr = str.split(" ");//с помощью метода split разделил строку на состовляющие и заполнил массив String
         for (int i = 0; i < arrstr.length; i++) {
             a = Integer.parseInt(arrstr[i]); //используем метод parseInt для конвертации переменных string в int и тут же заполняем массив
             if (a > 0) { //сравниваем с нулём и плюсуем счётчик, если положительное
