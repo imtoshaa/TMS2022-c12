f30b648ac0aad652dc49c73c86d5b40dda93e9db
Anton Auseichyk <imtoshaa.work@yandex.ru>
2022-02-03 15:21:42 +0300

    add Lesson7

--- Lesson6/src/main/java/Main.java
+++ Lesson6/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         User user1 = new User(30, "Иван", "Иванов", "Мужской");
@@ -6,5 +8,7 @@ public class Main {
         System.out.println(user1.information());
         User user = new User(20, "Антон", "Овсейчик", "Мужской");
         user.addAge(40);
+        ArrayList<String> objects = new ArrayList<>();
+        objects.add("asdasd");
     }
 }
