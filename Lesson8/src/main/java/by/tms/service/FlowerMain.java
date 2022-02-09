fcab33904da21f6953fca2ce500d8da35f2ce593
Anton Auseichyk <imtoshaa.work@yandex.ru>
2022-02-09 20:28:13 +0300

    add Lesson8

--- Lesson8/src/main/java/by/tms/service/FlowerMain.java
+++ Lesson8/src/main/java/by/tms/service/FlowerMain.java
@@ -10,7 +10,11 @@ public class FlowerMain {
     public static void main(String[] args) {
         FlowerMarket flowerMarket = new FlowerMarket();
         System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, HERBERA, LILY, ASTER, TULIP, ASTER, CARNATION));
-        System.out.println("Из количества цветов = " + flowerMarket.getCount());
-        System.out.println("Стоимость букета составила " + flowerMarket.getPrice() + " рублей");
+        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(HERBERA, ROSE, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION));
+        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, HERBERA, LILY, ASTER, TULIP, ASTER, ROSE));
+        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, CARNATION, LILY, ASTER, TULIP, ASTER, CARNATION));
+        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION));
+        System.out.println("Количество = " + flowerMarket.getCount());
+        System.out.println("Стоимость букетов составила " + flowerMarket.getPrice() + " рублей");
     }
 }
