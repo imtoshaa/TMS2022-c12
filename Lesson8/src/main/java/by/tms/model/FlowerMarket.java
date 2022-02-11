24bffdc007d3acc9d5e7dcb65c737d4e7daf1d03
Anton Auseichyk <imtoshaa.work@yandex.ru>
2022-02-11 18:53:09 +0300

    update

--- Lesson8/src/main/java/by/tms/model/FlowerMarket.java
+++ Lesson8/src/main/java/by/tms/model/FlowerMarket.java
@@ -1,19 +1,15 @@
 package by.tms.model;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 
 import static by.tms.utils.Constants.NAME_PRICE_FLOWERS;
 
-//@Getter
-//если я создаю @Getter тут на все поля, то у меня ломается программа, выводит, что цена букета равно нулю
-//если создаю руками, то всё ок
-//а если я сначала запускаю c аннотацией Getter, а потом запускаю с созданными геттерами вручную, то всё равно ломается. нужно чистить таргет
+@Getter
 public class FlowerMarket {
-    //    @Getter
     private int count;
-    //    @Getter
     private int cost;
-    //    @Getter
     private Bouquet bouquet;
 
     public Bouquet getBouquet(String... flowers) {
@@ -26,17 +22,5 @@ public class FlowerMarket {
         cost += bouquet.getCostBouquet();
         count += flowerArrayList.size();
         return bouquet;
-    }
-
-    public Bouquet getBouquet() {
-        return bouquet;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public int getCost() {
-        return cost;
     }
 }
--- Lesson8/src/main/java/by/tms/service/FlowerMain.java
+++ Lesson8/src/main/java/by/tms/service/FlowerMain.java
@@ -1,5 +1,6 @@
 package by.tms.service;
 
+import by.tms.model.Bouquet;
 import by.tms.model.FlowerMarket;
 
 import static by.tms.utils.Constants.*;
@@ -8,20 +9,26 @@ public class FlowerMain {
 
     public static void main(String[] args) {
         FlowerMarket flowerMarket = new FlowerMarket();
-        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, HERBERA, LILY, ASTER, TULIP, ASTER, CARNATION));
-        System.out.println("Цена букета в рублях = " + flowerMarket.getBouquet().getCostBouquet()); //полиморфм
 
-        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(HERBERA, ROSE, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION));
-        System.out.println("Цена букета в рублях = " + flowerMarket.getBouquet().getCostBouquet());
+        Bouquet bouquet = flowerMarket.getBouquet(ROSE, ROSE, HERBERA, LILY, ASTER, TULIP, ASTER, CARNATION);
+        System.out.println("Сформирован следующий букет: " + bouquet);
+        System.out.println("Цена букета в рублях = " + bouquet.getCostBouquet());
 
-        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, CARNATION, LILY, ASTER, TULIP, ASTER, CARNATION));
-        System.out.println("Цена букета в рублях = " + flowerMarket.getBouquet().getCostBouquet());
+        Bouquet bouquet1 = flowerMarket.getBouquet(HERBERA, ROSE, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION);
+        System.out.println("Сформирован следующий букет: " + bouquet1);
+        System.out.println("Цена букета в рублях = " + bouquet1.getCostBouquet());
 
-        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, HERBERA, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION));
-        System.out.println("Цена букета в рублях = " + flowerMarket.getBouquet().getCostBouquet());
+        Bouquet bouquet2 = flowerMarket.getBouquet(ROSE, ROSE, CARNATION, LILY, ASTER, TULIP, ASTER, CARNATION);
+        System.out.println("Сформирован следующий букет: " + bouquet2);
+        System.out.println("Цена букета в рублях = " + bouquet2.getCostBouquet());
 
-        System.out.println("Сформирован следующий букет: " + flowerMarket.getBouquet(ROSE, ROSE, HERBERA, LILY, TULIP, TULIP, ASTER, ROSE));
-        System.out.println("Цена букета в рублях = " + flowerMarket.getBouquet().getCostBouquet());
+        Bouquet bouquet3 = flowerMarket.getBouquet(ROSE, HERBERA, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION);
+        System.out.println("Сформирован следующий букет: " + bouquet3);
+        System.out.println("Цена букета в рублях = " + bouquet3.getCostBouquet());
+
+        Bouquet bouquet4 = flowerMarket.getBouquet(ROSE, HERBERA, HERBERA, ASTER, ASTER, TULIP, ASTER, CARNATION);
+        System.out.println("Сформирован следующий букет: " + bouquet4);
+        System.out.println("Цена букета в рублях = " + bouquet4.getCostBouquet());
 
         System.out.println("Стоимость всех букетов в рублях = " + flowerMarket.getCost());
         System.out.println("Количество проданых цветов = " + flowerMarket.getCount());
